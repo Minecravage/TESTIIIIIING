38a3b328d823d3b4121918f50d1b7db4684bac61
Minecravage <159172504+Minecravage@users.noreply.github.com>
2024-09-30 12:31:18 +0200

    Add

---
+++ service-worker.js
@@ -0,0 +1,8 @@
+self.addEventListener('install', function(event) {
+    // Cache des ressources lors de l'installation
+  });
+  
+  self.addEventListener('fetch', function(event) {
+    // Gestion des requêtes réseau
+  });
+  
